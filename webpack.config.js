64440e6ebe125981935a490dd0bca6908a6d6949
Lucas <lucas.edgerly@gmail.com>
2022-09-28 14:22:09 -0400

    add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const ReactRefresh = require('@pmmmwh/react-refresh-webpack-plugin')
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: path.resolve(__dirname, 'src','index.jsx'),
+  entry: path.resolve(__dirname, 'src','index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js','.jsx',]
+    extensions: ['.js','.jsx','.ts','.tsx']
   },
   plugins: [
     isDevelopment && new ReactRefresh(),
@@ -30,7 +30,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
